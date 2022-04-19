86e8bba20e85ee32158dc14846f2d2f8a50d0196
Vivek Shah <vivekshahcodes@gmail.com>
2022-04-19 14:20:29 +0530

    add comments

--- notes.js
+++ notes.js
@@ -1,6 +1,25 @@
 const fs = require('fs');
 const chalk = require('chalk');
 
+// Loads the notes from the JSON file and parses it into an array of objects.
+const loadNotes = () => {
+
+    try {
+        const dataBuffer = fs.readFileSync('notes.json');
+        const dataJSON = dataBuffer.toString();
+        return JSON.parse(dataJSON);
+    } catch (e) {
+        return []
+    }
+}
+
+//Converts the array of objects into JSON and saves it to the JSON file.
+const saveNotes = (notes) => {
+    const dataJSON = JSON.stringify(notes);
+    fs.writeFileSync('notes.json', dataJSON);
+}
+
+//Adds a note to the JSON file.
 const addNote = (title, body) => {
     const notes = loadNotes();
     const duplicateNote = notes.find((note) => note.title === title);
@@ -17,6 +36,30 @@ const addNote = (title, body) => {
     }
 }
 
+//Removes a note from the JSON file.
+const removeNote = (title) => {
+    const notes = loadNotes();
+    const notesToKeep = notes.filter((note) => note.title !== title);
+
+    if (notes.length > notesToKeep.length) {
+        console.log(chalk.green.inverse('Note Removed!'));
+        saveNotes(notesToKeep);
+    } else {
+        console.log(chalk.red.inverse('No Note Found!'));
+    }
+}
+
+//Lists all the notes.
+const listNotes = () => {
+    const notes = loadNotes();
+    console.log(chalk.inverse('Your notes'))
+
+    notes.forEach((note) => {
+        console.log(note.title);
+    })
+}
+
+//Prints out the title and body of a note.
 const readNote = (title) => {
 
     const notes = loadNotes();
@@ -30,43 +73,7 @@ const readNote = (title) => {
     }
 }
 
-const saveNotes = (notes) => {
-    const dataJSON = JSON.stringify(notes);
-    fs.writeFileSync('notes.json', dataJSON);
-}
-
-const loadNotes = () => {
-
-    try {
-        const dataBuffer = fs.readFileSync('notes.json');
-        const dataJSON = dataBuffer.toString();
-        return JSON.parse(dataJSON);
-    } catch (e) {
-        return []
-    }
-}
-
-const removeNote = (title) => {
-    const notes = loadNotes();
-    const notesToKeep = notes.filter((note) => note.title !== title);
-
-    if (notes.length > notesToKeep.length) {
-        console.log(chalk.green.inverse('Note Removed!'));
-        saveNotes(notesToKeep);
-    } else {
-        console.log(chalk.red.inverse('No Note Found!'));
-    }
-}
-
-const listNotes = () => {
-    const notes = loadNotes();
-    console.log(chalk.inverse('Your notes'))
-
-    notes.forEach((note) => {
-        console.log(note.title);
-    })
-}
-
+// Exports the functions into the app.js file.
 module.exports = {
     addNote: addNote,
     removeNote: removeNote,
